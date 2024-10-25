a7b330cb70b34ae114ae1ee44508a1dae43e8414
kuwa124 <c0mwgave1407@gmail.com>
2024-10-25 12:25:45 +0900

    🐛 アナウンスのタイミングが間違っていた為、修正

--- src/hooks/useAnnounce.ts
+++ src/hooks/useAnnounce.ts
@@ -8,37 +8,39 @@ type UseAnnounce = {
 export const useAnnounce = (): UseAnnounce => {
   // 音声を再生する関数
   const announce = async (audioFile: string): Promise<void> => {
-    try {
-    const audio = new Audio(`/audio/${audioFile}`);
+    // try {
+      const audio = new Audio(`/audio/${audioFile}`);
 
-    return new Promise<void>((resolve, reject) => {
-      audio.onended = () => resolve();
-      audio.onerror = () => reject(new Error('音声の再生に失敗しました'));
-      audio.play().catch(reject);
-    });
-    } catch (error) {
-      console.error('音声再生エラー:', error);
-      throw error;
-    }
+      return new Promise<void>((resolve, reject) => {
+        audio.onended = () => resolve();
+        audio.onerror = () => reject(new Error('音声の再生に失敗しました'));
+        audio.play().catch(reject);
+      });
+    // } catch (error) {
+    //   console.error('音声再生エラー:', error);
+    //   throw error;
+    // }
   };
 
   // アナウンスのタイミングをチェックする関数
   const checkAnnouncements = useCallback(
     (time: number) => {
       const announcements = [
-        { time: 3002 - 10 * 60, file: '03_10minProgress.wav' },
-        { time: 3002 - 35 * 60, file: '04_35minProgress.wav' },
-        { time: 3002 - 55 * 60, file: '05_5minutesAgo.wav' },
-        { time: 3002 - 57 * 60, file: '06_3minutesAgo.wav' },
-        { time: 3002 - 59 * 60, file: '07_1minutesAgo.wav' },
+        // 50分（3000秒）から経過した分数後にアナウンス
+        // 何分経過後のアナウンスか、わかりやすいように「○○ * 60」で記載
+        { time: 3000 - 10 * 60, file: '03_10minProgress.wav' },
+        { time: 3000 - 35 * 60, file: '04_35minProgress.wav' },
+        { time: 3000 - 45 * 60, file: '05_5minutesAgo.wav' },
+        { time: 3000 - 47 * 60, file: '06_3minutesAgo.wav' },
+        { time: 3000 - 49 * 60, file: '07_1minutesAgo.wav' },
       ];
 
       const announcement = announcements.find((a) => a.time === time);
-      if (announcement) {
-        announce(announcement.file).catch((error) => {
-          console.error('アナウンスの再生に失敗しました:', error);
-        });
-      }
+      // if (announcement) {
+      //   announce(announcement.file).catch((error) => {
+      //     console.error('アナウンスの再生に失敗しました:', error);
+      //   });
+      // }
     },
     [announce]
   );
