21a49209897f8124c8a29a854bd269b38f89391a
kuwa124 <c0mwgave1407@gmail.com>
2024-11-06 16:02:00 +0900

    💡 ✨ AudioCacheタイプとコメントを追加＆修正

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,4 +1,4 @@
-// 再生する音声ファイルの型
+/** 再生する音声ファイルの型 */
 export type Announcoments = {
   /** 経過時間（秒） */
   time: number;
@@ -6,7 +6,16 @@ export type Announcoments = {
   file: string;
 };
 
-// ナビゲーションボタンの型定義
+/**
+ * 音声ファイルをキャッシュするための型定義
+ * - キー: 音声ファイル名（string型）
+ * - 値: 音声データ（HTMLAudioElement型）
+ */
+type AudioCache = {
+  [key: string]: HTMLAudioElement;
+};
+
+/** ナビゲーションボタンの型定義 */
 export type NavigationButton = {
   /** 画面に表示する文字列 */
   title: string;
@@ -14,11 +23,13 @@ export type NavigationButton = {
   path: string;
 };
 
-// ページ設定の定義の型
-// Readonly<T>で全体を包むことで、設定値の変更を防ぐ（読み取り専用になる）
-// https://typescriptbook.jp/reference/type-reuse/utility-types/readonly
-// 参考サイト：サバイバルTypeScript
+/**
+ * ページ設定の定義の型
+ * - Readonly<T>で全体を包むことで、設定値の変更を防ぐ（読み取り専用になる）
+ * - https://typescriptbook.jp/reference/type-reuse/utility-types/readonly
+ * - 参考サイト：サバイバルTypeScript
+ * - [key: string]は、任意の文字列をキーとして使えることを示す
+ */
 export type PageConfig = Readonly<{
-  // [key: string]は、任意の文字列をキーとして使えることを示す
   [key: string]: Readonly<NavigationButton>;
 }>;
