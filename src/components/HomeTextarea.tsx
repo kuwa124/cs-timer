2effe2843d3f19c9362ad693240ea6f925bc9e5a
kuwa124 <c0mwgave1407@gmail.com>
2024-11-27 10:26:04 +0900

    🎨 ♻️ 🚸  アコーディオンの見出しクリック開閉と細かなリファクタリング

--- src/components/HomeTextarea.tsx
+++ src/components/HomeTextarea.tsx
@@ -15,30 +15,21 @@ export const HomeTextarea = () => {
   const [isTest, setIsTest] = useState<boolean>(false);
 
   return (
-    <div className='space-y-8'>
+    <div className='space-y-4'>
       <NavigationButtons
         handleClick={() => setisTestingBefore((prev) => !prev)}
         isTestingBefore={isTestingBefore}
         navigationButtons={testingBeforeNavigationButtons}
-      >
-        CS検定
-        <span className='text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-800'>
-          練習
-        </span>
-        　時間計測
-      </NavigationButtons>
+        highlightText='練習'
+      />
 
       <NavigationButtons
         handleClick={() => setIsTest((prev) => !prev)}
         isTestingBefore={isTest}
         navigationButtons={testNavigationButtons}
-      >
-        CS検定
-        <span className='text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-800'>
-          試験
-        </span>
-        　時間計測
-      </NavigationButtons>
+        className='pt-8 border-y-4'
+        highlightText='試験'
+      />
     </div>
   );
 };
--- src/components/NavigationButtons.tsx
+++ src/components/NavigationButtons.tsx
@@ -1,31 +1,38 @@
 import { NavigationButton } from '@/types/type';
 import { ChevronDown } from 'lucide-react';
 import Link from 'next/link';
-import { ReactNode } from 'react';
 
 type NavigationButtonsProps = {
   handleClick: () => void;
   isTestingBefore: boolean;
   navigationButtons: NavigationButton[];
-  children: string | ReactNode;
+  highlightText: string;
+  className?: string;
 };
 
 export const NavigationButtons = ({
   handleClick,
   isTestingBefore,
   navigationButtons,
-  children,
+  highlightText,
+  className,
 }: NavigationButtonsProps) => {
   return (
-    <div className=' my-4 space-y-4 w-[600px]'>
-      <div className='flex justify-between items-center pb-8 border-b-4'>
-        <button className='text-2xl sm:text-3xl lg:text-4xl font-semibold text-gray-600'>
-          {children}
+    <div className=' my-4 w-[600px]'>
+      <div
+        className={`flex justify-between items-center pb-8 ${
+          isTestingBefore && 'border-b-4'
+        } ${className}`}
+        onClick={handleClick}
+      >
+        <button className='group text-2xl sm:text-3xl lg:text-4xl font-semibold text-gray-600 hover:text-orange-600'>
+          CS検定
+          <span className='text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-800 group-hover:text-orange-600'>
+            {highlightText}
+          </span>
+          　時間計測
         </button>
-        <button
-          className='mr-2 rounded-full hover:bg-gray-300 p-4 cursor-pointer'
-          onClick={handleClick}
-        >
+        <button className='mr-2 rounded-full hover:bg-gray-300 p-4 cursor-pointer'>
           <ChevronDown
             size={24}
             className={`text-gray-700 ${isTestingBefore && 'rotate-180'}`}
@@ -36,12 +43,12 @@ export const NavigationButtons = ({
       {/* ` TestingBefore ` のボタン部分を展開 */}
       {/* grid-flow-col:列に縦並びで次の列に進む */}
       {isTestingBefore && (
-        <nav className='grid grid-rows-3 grid-flow-col gap-y-4 gap-x-10 text-gray-600'>
+        <nav className='grid grid-rows-4 grid-flow-col gap-y-4 gap-x-10 mt-4 text-gray-600'>
           {navigationButtons.map((navi) => (
             <Link
               key={navi.path}
               href={navi.path}
-              className='w-56 mx-auto px-8 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition'
+              className='w-[245px] mx-auto px-8 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition'
               aria-label={`${Object.values(navi.titleConfig).join('')}へ移動`} //titleConfigを連結してタイトル作成
             >
               {/* titleConfigを連結してタイトル作成 */}
